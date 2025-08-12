4da39ab4c9d85af19de6920480e1f223e95c73cc
SAYANDIP SAHA <sayandiplynn@gmail.com>
2025-08-12 16:55:02 +0530

    Fixed

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT
-    ).then(() => {
-        console.log('Connected to DB');
-    }).catch(err => console.log(err));
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("✅ Connected to MongoDB Atlas"))
+    .catch((err) =>
+      console.error("❌ Error connecting to MongoDB Atlas:", err)
+    );
 }
-
 
 module.exports = connectToDb;
